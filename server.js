d9a409b2cdea0a331a735f5be32f076fbe8fec4e
DucSenSei <115126370+DucSenSei@users.noreply.github.com>
2023-03-15 23:01:06 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const multer = require('multer');
+const uuid = require('uuid').v4;
+
+const fileupload = multer({ dest: 'fileupload'});
+
+const app = express();
+app.use(express.static('axxx'));
+
+app.post('/fileupload',fileupload.single('avatar'),(req,res) =>{
+    return res.json({status: 'OK'});
+})
+
+app.listen(3001);
